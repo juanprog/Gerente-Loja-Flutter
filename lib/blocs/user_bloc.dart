ce89fec57667a18d230810ce912a63322f2ad12a
Juan Carlos <jcguitar1994@gmail.com>
2020-04-17 11:44:48 -0300

    Create UserBlock class

---
+++ lib/blocs/user_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+
+class UserBlock extends BlocBase {
+  final _usersController = BehaviorSubject();
+
+  Map<String, Map<String, dynamic>> _users = {};
+
+  Firestore _firestore = Firestore.instance;
+
+  UserBlock() {
+    _addUsersListener();
+  }
+
+  void _addUsersListener() {
+    _firestore.collection("users").snapshots().listen((snapshot) {
+      snapshot.documentChanges.forEach((change) {
+        String uid = change.document.documentID;
+
+        switch (change.type) {
+          case DocumentChangeType.added:
+            _users[uid] = change.document.data;
+            break;
+          case DocumentChangeType.modified:
+            _users[uid].addAll(change.document.data);
+            break;
+          case DocumentChangeType.removed:
+            _users.remove(uid);
+            break;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _usersController.close();
+  }
+}
