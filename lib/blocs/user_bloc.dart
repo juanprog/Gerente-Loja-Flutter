f274ead9c89375976582712e6430f006c69a6e6b
Juan Carlos <jcguitar1994@gmail.com>
2020-04-18 10:03:31 -0300

    Create function that display users orders information

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -33,6 +33,31 @@ class UserBlock extends BlocBase {
     });
   }
 
+  void _subscribeToOrders(String uid) {
+    _firestore
+        .collection("users")
+        .document(uid)
+        .collection("orders")
+        .snapshots()
+        .listen((orders) async {
+      int numOrders = orders.documents.length;
+      double money = 0.0;
+
+      for (DocumentSnapshot d in orders.documents) {
+        DocumentSnapshot order =
+            await _firestore.collection("orders").document(d.documentID).get();
+
+        if (order.data == null) continue;
+
+        money += order.data["totalPrice"];
+      }
+
+      _users[uid].addAll({"money": money, "orders": numOrders});
+
+      _usersController.add(_users.values.toList());
+    });
+  }
+
   @override
   void dispose() {
     _usersController.close();
