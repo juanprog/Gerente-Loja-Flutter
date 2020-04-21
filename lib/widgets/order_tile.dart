f9a963432146d31b126e7c46ac7734a4a5b31068
Juan Carlos <jcguitar1994@gmail.com>
2020-04-21 09:21:26 -0300

    Create initial card layout

--- lib/widgets/order_tile.dart
+++ lib/widgets/order_tile.dart
@@ -3,6 +3,60 @@ import 'package:flutter/material.dart';
 class OrderTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      child: Card(
+        child: ExpansionTile(
+          title: Text(
+            "#12345 - Entregue",
+            style: TextStyle(color: Colors.green),
+          ),
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        title: Text("Camiseta Preta P"),
+                        subtitle: Text("Camisetas de alta qualidade!"),
+                        trailing: Text(
+                          "2",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        contentPadding: EdgeInsets.zero,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FlatButton(
+                        onPressed: () {},
+                        textColor: Colors.red,
+                        child: Text("Excluir"),
+                      ),
+                      FlatButton(
+                        onPressed: () {},
+                        textColor: Colors.grey[850],
+                        child: Text("Regredir"),
+                      ),
+                      FlatButton(
+                        onPressed: () {},
+                        textColor: Colors.green,
+                        child: Text("Avançar"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
