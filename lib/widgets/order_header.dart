f0a5ce535065701f1e5a57dd43829fa69b6371aa
Juan Carlos <jcguitar1994@gmail.com>
2020-04-21 09:53:04 -0300

    Add header itens

--- lib/widgets/order_header.dart
+++ lib/widgets/order_header.dart
@@ -3,6 +3,35 @@ import 'package:flutter/material.dart';
 class OrderHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Daniel"),
+              Text("Rua Flutter Top"),
+            ],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "Preço dos Produtos",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              "Preço Total",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
