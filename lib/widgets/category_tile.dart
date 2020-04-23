9211953d0a0f937bd9852f4c321ce6a13aedbcc8
Juan Carlos <jcguitar1994@gmail.com>
2020-04-23 11:03:53 -0300

    Create CategoryTile widget

---
+++ lib/widgets/category_tile.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryTile extends StatelessWidget {
+  final DocumentSnapshot category;
+
+  CategoryTile(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
