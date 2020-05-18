e9a929a5ff63dc12fc30fa7e663176e86a274117
Juan Carlos <jcguitar1994@gmail.com>
2020-05-18 15:23:12 -0300

    Create save product function

--- lib/blocs/products_bloc.dart
+++ lib/blocs/products_bloc.dart
@@ -1,11 +1,14 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:rxdart/rxdart.dart';
 
 class ProductBloc extends BlocBase {
   final _dataController = BehaviorSubject<Map>();
+  final _loadingController = BehaviorSubject<bool>();
 
   Stream<Map> get outData => _dataController.stream;
+  Stream<bool> get outLoading => _loadingController.stream;
 
   String categoryId;
   DocumentSnapshot product;
@@ -46,8 +49,53 @@ class ProductBloc extends BlocBase {
     unsavedData["images"] = images;
   }
 
+  Future<bool> saveProduct() async {
+    _loadingController.add(true);
+
+    try {
+      if (product != null) {
+        await _uploadImages(product.documentID);
+        await product.reference.updateData(unsavedData);
+      } else {
+        DocumentReference dr = await Firestore.instance
+            .collection("products")
+            .document("categoryId")
+            .collection("items")
+            .add(Map.from(unsavedData)..remove("images"));
+
+        await _uploadImages(dr.documentID);
+        await dr.updateData(unsavedData);
+      }
+
+      _loadingController.add(false);
+      return true;
+    } catch (e) {
+      _loadingController.add(false);
+      return false;
+    }
+  }
+
+  Future _uploadImages(String productId) async {
+    for (int i = 0; i < unsavedData["images"].length; i++) {
+      if (unsavedData["images"][i] is String) continue;
+
+      StorageUploadTask uploadTask = FirebaseStorage.instance
+          .ref()
+          .child(categoryId)
+          .child(productId)
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(unsavedData["images"][i]);
+
+      StorageTaskSnapshot s = await uploadTask.onComplete;
+      String downloadUrl = await s.ref.getDownloadURL();
+
+      unsavedData["images"][i] = downloadUrl;
+    }
+  }
+
   @override
   void dispose() {
-    _dataController.distinct();
+    _dataController.close();
+    _loadingController.close();
   }
 }
