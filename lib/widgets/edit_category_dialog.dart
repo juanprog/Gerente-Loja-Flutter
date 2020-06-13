ee038adb5a0bf739e34b3176ce3faf6fe46fd73b
Juan Carlos <jcguitar1994@gmail.com>
2020-06-13 16:47:19 -0300

    Add Dialog elements

--- lib/widgets/edit_category_dialog.dart
+++ lib/widgets/edit_category_dialog.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class EditCategoryDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Dialog(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: GestureDetector(
+                child: CircleAvatar(),
+              ),
+              title: TextField(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FlatButton(
+                  child: Text("Excluir"),
+                  textColor: Colors.red,
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  child: Text("Salvar"),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
